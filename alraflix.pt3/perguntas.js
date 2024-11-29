d9588b1814b45f6d14c5eb6665efa8570d6efdf0
Coa23062007 <00001074925671sp@al.educacao.sp.gov.br>
2024-11-29 11:27:52 -0300

    Add files via upload

---
+++ alraflix.pt3/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'qual é o melhor time da história?',
+    'O melhor time é o (corinthians)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Bahia?',
+    'A capital da Bahia é (Salvador)'
+)
+
+criaCartao(
+    'Escola',
+    'oque você acha da escola?',
+    'A resposta certa é (Uma bosta que não deveria existir)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz qua o seu nome em Inglês?',
+    'Oi em ingles é (what is your name)'
+)
